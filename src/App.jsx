0c9d6540b553ce75e2cbc727304fbc62fbdab694
Utkarsh-bhosale <bhosaleutkarsh13@gmail.com>
2025-05-01 12:53:41 +0530

    completed the project by adding functionality to copy button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useCallback, useEffect } from 'react'
+import { useState , useCallback, useEffect , useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
@@ -7,6 +7,9 @@ function App() {
   const [numberAllowed , setNumberAllowed] = useState(false);
   const [charAllowed , setCharAllowed] = useState(false);
   const [password , setPassword] = useState("");
+
+  //useRef 
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
@@ -24,6 +27,11 @@ function App() {
 
   }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select()
+    window.navigator.clipboard.writeText(password)
+  },[password])
+
   useEffect(() => {
     passwordGenerator()
   }, [length , numberAllowed , charAllowed , passwordGenerator])
@@ -40,9 +48,10 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref = {passwordRef}
           />
           <button
-            //onClick={copyPasswordToClipboard}
+            onClick={copyPasswordToClipboard}
             className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
           >
             Copy
